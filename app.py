03c27902831c1683d6e9c4430869629a07eb2d95
MilkCatt <marcuschaoy@gmail.com>
2024-04-30 13:06:54 -0700

    updating to hackathon version

--- app.py
+++ app.py
@@ -9,8 +9,10 @@ from langchain_openai import ChatOpenAI
 from langchain.chains import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
+from langchain.chains.query_constructor.base import AttributeInfo
+# from langchain.retrievers.self_query.base import SelfQueryRetriever
 # from langchain import hub
-from db import load_db
+from db import load_db, load_listings
 
 
 load_dotenv()
@@ -25,10 +27,26 @@ text_splitter = CharacterTextSplitter(
 )
 
 SYSTEM_PROMPT = (
-    "You are a helpful AI car salesman. A user is looking for a car. "
-    "The provided context is a dataset containing car models that is related to the preferences of the user. "
-    "The dataset includes the specific car model and the summarized review of the particular car. "
-    "Please analyse the cars and its reviews and preferences of the users and return the best 5 cars they should consider. "
+    "You are a helpful AI car salesman. You are helping a user buy a car suited for them."
+    "The provided context is a dataset containing a list of car models. "
+    "The dataset includes the specific car model and the list of car reviews. "
+    "Using the dataset of car models, suggest the most suitable 20 types of cars from the dataset for the user. "
+    "You must reply with 20 types of cars, from the dataset."
+    "For each of these, also include a summary of the reviews for each car, and an explanation of why it best fits the user query"
+    "----------------"
+    "{context}"
+    "----------------"
+)
+
+SYSTEM_PROMPT2 = (
+    "You are a helpful AI car salesman. You are helping a user buy a car suited for them."
+    "The provided context is a set of current car listings from Craigslist. "
+    "Using the dataset of car models, suggest the most suitable 5 cars from the dataset for the user. "
+    "Provide a summarised version of the car listing, including the location, title, link and price of the car."
+    "If you are unable to provide the specific cars listed, instead provide similar cars which can be found in the car listings."
+    "Include the pros and cons of this car, specific to the user's requirements."
+    "Also include brief summary of the reviews about the car."
+    "You must reply with 5 car listings, from the Craigslist listings."
     "----------------"
     "{context}"
     "----------------"
@@ -38,8 +56,68 @@ PROMPT = ChatPromptTemplate.from_messages(
     [("system", SYSTEM_PROMPT), ("human", "{input}")]
 )
 
+PROMPT2 = ChatPromptTemplate.from_messages(
+    [("system", SYSTEM_PROMPT2), ("human", "{input}")]
+)
+
 # app = Flask(__name__)
 # CORS(app)
+
+metadata_field_info_db1 = [
+    AttributeInfo(
+        name="Vehicle_Title",
+        description="The name and model of the car",
+        type="string",
+    ),
+    AttributeInfo(
+        name="Review",
+        description="A series of reviews about the car. Separate reviews are delimited by ||",
+        type="String",
+    ),
+]
+
+document_content_description = "Set of car reviews"
+
+metadata_field_info_db2 = [
+    AttributeInfo(
+        name="neighbourhood",
+        description="The name and model of the car",
+        type="string",
+    ),
+    AttributeInfo(
+        name="post title",
+        description="A series of reviews about the car. Separate reviews are delimited by ||",
+        type="String",
+    ),
+    AttributeInfo(
+        name="URL",
+        description="A series of reviews about the car. Separate reviews are delimited by ||",
+        type="String",
+    ),
+    AttributeInfo(
+        name="price",
+        description="A series of reviews about the car. Separate reviews are delimited by ||",
+        type="Integer",
+    ),
+]
+document_content_description2 = "Set of Craigslist listings"
+
+# llm = ChatOpenAI(temperature=0)
+
+# retriever = SelfQueryRetriever.from_llm(
+#     llm,
+#     vectorstore,
+#     document_content_description,
+#     metadata_field_info,
+# )
+
+# retriever = SelfQueryRetriever.from_llm(
+#     llm,
+#     self.load_db(),
+#     document_content_description,
+#     metadata_field_info,
+#     search_kwargs={"k": 10},verbose=True
+# )
 
 
 def prompt(query):
@@ -47,9 +125,10 @@ def prompt(query):
     print(query)
 
     retriever = load_db()
+    retriever2 = load_listings()
     # model = Ollama(base_url='http://119.74.32.2:11434',
     #                model="llama2", temperature=0.8)
-    model = ChatOpenAI(model="gpt-3.5-turbo",
+    model = ChatOpenAI(model="gpt-4-turbo-2024-04-09",
                        api_key=os.getenv("OPENAI_API_KEY"), temperature=0)
 
     # qachain = RetrievalQA.from_chain_type(model, retriever=retriever)
@@ -58,13 +137,19 @@ def prompt(query):
         model, PROMPT)
     chain = create_retrieval_chain(retriever, question_answer_chain)
 
+    question_answer_chain2 = create_stuff_documents_chain(
+        model, PROMPT2)
+    chain2 = create_retrieval_chain(retriever2, question_answer_chain2)
+
     output = chain.invoke({"input": query})
+    output2 = chain2.invoke({"input": output["answer"]})
     # output = qachain.invoke({"query": prompt})
     print(output)
-    return {"response": output["answer"]}
+    print(output2)
+    return {"response": output2["answer"]}
 
 
 st.title("📓 Matt's Black Book")
-if resp := st.text_input("Enter your query here", key="query"):
+if resp := st.text_area("Enter your query here", key="query"):
     response = prompt(resp)
     st.write(response["response"])
--- db.py
+++ db.py
@@ -14,7 +14,7 @@ load_dotenv()
 set_verbose(True)
 
 embedding_function = SentenceTransformerEmbeddings(
-    model_name="all-MiniLM-L6-v2")
+    model_name="all-MiniLM-L6-v2", show_progress=True)
 # embedding_function = OpenAIEmbeddings(
 #     model="text-embedding-3-small", api_key=os.getenv("OPENAI_API_KEY"), chunk_size=1000, show_progress_bar=True)
 
@@ -25,10 +25,22 @@ def init_db():
     data = loader.load()
     print("Embedding")
     # db = Chroma.from_documents(data, embedding_function)
-    print("Done embedding")
     # db.save_local("reviews")
     db = Chroma.from_documents(
         data, embedding_function, persist_directory="./chroma_db")
+    print("Done embedding")
+    return db
+
+def init_listings():
+    loader = CSVLoader(file_path='./data/list.csv',
+                       source_column="URL")
+    data = loader.load()
+    print("Embedding")
+    # db = Chroma.from_documents(data, embedding_function)
+    # db.save_local("reviews")
+    db = Chroma.from_documents(
+        data, embedding_function, persist_directory="./cars_db")
+    print("Done embedding")
     return db
 
 
@@ -41,6 +53,15 @@ def load_db():
 
     return retriever
 
+def load_listings():
+    # db = FAISS.load_local("reviews", embedding_function,
+    #                       allow_dangerous_deserialization=True)
+    db = Chroma(persist_directory="./cars_db",
+                embedding_function=embedding_function)
+    retriever = db.as_retriever()
+
+    return retriever
+
 
 if __name__ == "__main__":
-    init_db()
+    init_listings()
